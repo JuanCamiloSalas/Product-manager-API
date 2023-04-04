5c5bafca07294447a137886cf6df63cd53aef80e
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-04 13:32:47 -0500

    feat(API): :rocket: index de la API

---
+++ index.js
@@ -0,0 +1,9 @@
+const app = require("./src/app.js");
+const { conn } = require("./src/db.js");
+
+const port = process.env.PORT || 3001;
+conn.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`Server on port ${port}`);
+    });
+});
