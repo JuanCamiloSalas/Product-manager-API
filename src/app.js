14df8b8e757361b533db6a74064bce62fa4e89b8
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-04 18:27:12 -0500

    fix(API): :bug: Prueba: solución al problema de permisos

--- src/app.js
+++ src/app.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
+app.options('*', cors({
+    allowedHeaders: ['x-auth-token']
+}));
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
 
 // Routes
 app.use("/api", routes);
--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-    return res.send("/auth");
+    res.send("/auth");
 });
 
 module.exports = router;
