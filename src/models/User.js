089143529eb96c741d113d8d595802258e507a14
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-04 13:35:58 -0500

    feat(models): :rocket: Definición del modelo User

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+    sequelize.define("user", {
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        role: {
+            type: DataTypes.ENUM('user', 'admin'),
+            defaultValue: 'user',
+        }
+    });
+}
