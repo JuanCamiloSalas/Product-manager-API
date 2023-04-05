2bdc7467b4aadeb0c558b88152188b43ce3e3294
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-05 11:44:57 -0500

    feat(middlewares): :rocket: Middleware de autorización

---
+++ src/middlewares/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authorization = (req, res, next) => {
+    // Leer tóken del header
+    const token = req.header('x-auth-token');
+
+    // Revisar si no hay tóken
+    if (!token) return res.status(401).json({errors: [{msg: "Envíe un tóken"}]});
+
+    // Validar el tóken
+    try {
+        const encryption = jwt.verify(token, process.env.SECRETA);
+        req.user = encryption.user;
+        next();
+    } catch (error) {
+        res.status(401).json({errors: [{msg: "Tóken inválido"}]})
+    }
+}
+
+module.exports = authorization;
