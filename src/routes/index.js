4f6d2a2f222bd392585dfa9c0c7705bfd13ea4bf
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-05 11:41:10 -0500

    feat(routes): :sparkles: Autenticación en rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,16 @@ const { Router } = require("express");
 
 const auth = require("./auth/index.js");
 const admin = require("./admin/index.js");
+const categories = require("./categories/index.js");
 const products = require("./products/index.js");
 
 const router = Router();
 
+const authorization = require("../middlewares/auth.js");
+
 router.use("/auth", auth);
-router.use("/admin",admin);
-router.use("/products",products);
+router.use("/admin", authorization, admin);
+router.use("/categories", authorization, categories);
+router.use("/products", products);
 
 module.exports = router;
