ee21f0abec8990cc544b36adca06c93f0784e234
JuanCamiloSalas <juansalasprogrammer@gmail.com>
2023-04-07 20:56:35 -0500

    feat(API): :zap: Validaciones de la ruta /products/csv

--- src/controllers/products/createProducts.js
+++ src/controllers/products/createProducts.js
@@ -3,12 +3,20 @@ const { Product } = require("../../db.js");
 const createProducts = async(req, res) => {
     try {
         const { productsList } = req;
-        const productsCount = productsList.length;
 
         Promise.allSettled(productsList)
             .then(product => product.map(el => el.value))
-            .then(values => values.forEach(el => Product.create(el)))
-            .then(response => res.status(200).json({ msg: `Se han creado ${productsCount} registros`}))
+            .then(values => values.forEach(el => Product.findOrCreate({
+                where: {
+                    name: el.name,
+                },
+                defaults: {
+                    description: el.description,
+                    price: el.price,
+                    categoryId: el.categoryId,
+                }
+            })))
+            .then(response => res.status(200).json({ msg: `Se han creado los registros`}))
             .catch(error => { 
                 throw new Error(error)
             })
--- src/middlewares/convertCSV.js
+++ src/middlewares/convertCSV.js
@@ -1,21 +1,67 @@
-const convertCSV = (req, res, next) => {
+const { Category } = require("../db.js");
+
+const convertCSV = async(req, res, next) => {
     try {
         const { table } = req;
         
+        // Separar cabecera de las demás filas
         const allLines = table.replace(/\r/g, '').split('\n');
         const header = allLines[0];
-        const dataLines = allLines.slice(1);
-
+        const dataLines = allLines.slice(1); 
         const fieldNames = header.split(',');
 
+        // Validar que la tabla sea de 4 columnas
+        if (fieldNames.length !== 4) {
+            return res.status(400).json({ msg: `La tabla debe tener 4 columnas`});
+        }
+
+        // Validar que tenga los campos exigidos
+        const requiredHeaders = ['name', 'description', 'price', 'categoryId'];
+        for (const el of requiredHeaders) {
+            if (!fieldNames.includes(el)) {
+                return res.status(400).json({ msg: `La columna ${el} es requerida`});
+            }
+        }
+
+        // Buscar todos los Ids de las categorías
+        const categories = await Category.findAll({
+            attributes: ['id'], 
+        });
+        const categoriesIds = categories.map(el => el.dataValues.id);
+
+
+        // Crear los objetos de cada producto
         let productsList = [];
         for (let i = 0; i < dataLines.length; i++) {
             let obj = {};
             const data = dataLines[i].split(',');
 
             for (let j = 0; j < fieldNames.length; j++) {
+
+                let dataEl = data[j];
                 const fielName = fieldNames[j];
-                obj[fielName] = data[j];
+
+                if (fielName === 'price') { 
+
+                    dataEl = Number(dataEl);
+                    if (Number.isNaN(dataEl)) 
+                        return res.status(400).json({ msg: `price debe ser de tipo number`});
+
+                } else if (fielName === 'categoryId') {
+
+                    dataEl = String(dataEl).trim();
+                    if (!categoriesIds.includes(dataEl)) 
+                        return res.status(404).json({ msg: `El categoryId ${dataEl} No existe en la base de datos`});
+                
+                } else {
+                    dataEl = String(dataEl).trim();
+                }
+
+                if (String(dataEl).trim().length <= 0) {
+                    return res.status(400).json({ msg: `No se permiten campos vacíos`});
+                }
+
+                obj[fielName] = dataEl;
             }
 
             productsList.push(obj);
